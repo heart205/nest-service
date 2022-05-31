d3464b0fb5cd6d37e255b5d3c91399ef670811af
heart <7362469@qq.com>
2022-05-31 17:23:58 +0800

    chore: update port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-
+import { readEnvFile } from './utils/readFileEnv'
+interface env {
+  PORT?: string
+}
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(4000)
+  const env: env = readEnvFile(
+    process.cwd() + `/.${process.env.NODE_ENV || 'development'}.env`,
+  )
+  const port = env.PORT || 3664
+  await app.listen(port)
 }
 bootstrap()
